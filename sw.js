e0db6f97d5ca8eb80e541ed1641ec9d595fb40e9
loictiomo9-art <loictiomo9@gmail.com>
2026-07-09 14:58:02 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,47 @@
+// sw.js
+const CACHE_NAME = 'echecs-1800-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './styles.css',
+  './engine.js',
+  './ai.js',
+  './main.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
